89dff4e6de90e0eb85a0e5b8c72a007399665cb5
duc68456 <duc68456@gmail.com>
2024-12-09 22:11:42 +0700

    Add files via upload

---
+++ BLL/BookBLL.cs
@@ -0,0 +1,49 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class BookBLL
+    {
+        private readonly BookDAL bookDAL = new BookDAL();
+
+        public void AddBook(BookDTO book)
+        {
+            if (string.IsNullOrEmpty(book.Title))
+                throw new Exception("Tên sách không được để trống.");
+            bookDAL.AddBook(book);
+        }
+
+        public void UpdateBook(BookDTO book)
+        {
+            bookDAL.UpdateBook(book);
+        }
+        public void DeleteBook(int bookId)
+        {
+            if (bookId <= 0)
+                throw new Exception("Mã sách không hợp lệ.");
+            bookDAL.DeleteBook(bookId);
+        }
+        public List<BookDTO> GetAllBooks()
+        {
+            return bookDAL.GetAllBooks();
+        }
+
+        //
+        public List<BookDTO> GetBooks()
+        {
+            return bookDAL.GetBooks();
+        }
+        public BookDTO GetBookById(int bookId)
+        {
+            return bookDAL.GetBookById(bookId);
+        }
+
+    }
+}
---
+++ BLL/ReaderBLL.cs
@@ -0,0 +1,52 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class ReaderBLL
+    {
+        private ReaderDAL readerDAL = new ReaderDAL();
+
+        public List<ReaderDTO> GetAllReaders()
+        {
+            return readerDAL.GetAllReaders();
+        }
+
+        public bool AddReader(ReaderDTO reader)
+        {
+            if (string.IsNullOrEmpty(reader.Name) || string.IsNullOrEmpty(reader.IDNumber))
+            {
+                throw new ArgumentException("Name and IDNumber are required.");
+            }
+            return readerDAL.AddReader(reader);
+        }
+
+        public void UpdateReader(ReaderDTO reader)
+        {
+            if (reader.ReaderId <= 0 || string.IsNullOrEmpty(reader.Name) || string.IsNullOrEmpty(reader.IDNumber))
+                throw new Exception("Dữ liệu độc giả không hợp lệ.");
+
+            readerDAL.UpdateReader(reader); // Gọi hàm cập nhật từ DAL
+        }
+
+        public void DeleteReader(int readerId)
+        {
+            if (readerId <= 0)
+                throw new Exception("Mã độc giả không hợp lệ.");
+            readerDAL.DeleteReader(readerId); // Gọi hàm xóa từ DAL
+        }
+        public ReaderDTO GetReaderById(int readerId)
+        {
+            if (readerId <= 0)
+                throw new Exception("Mã độc giả không hợp lệ.");
+
+            return readerDAL.GetReaderById(readerId);
+        }
+    }
+}
