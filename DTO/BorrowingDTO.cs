f49424175e687753392ee6aaaf14fefaf8c83427
duc68456 <duc68456@gmail.com>
2024-12-09 22:13:08 +0700

    Add files via upload

---
+++ DTO/BorrowingDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class BorrowingDTO
+    {
+        public int BorrowingId { get; set; }
+        public int BookId { get; set; }
+        public int ReaderId { get; set; }
+        public DateTime BorrowDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+    }
+}
